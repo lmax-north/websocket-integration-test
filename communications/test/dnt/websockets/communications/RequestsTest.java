bef8a84e1afed4f84a43bf01f2ff405e759e02c4
David North <northd@lmax.com>
2025-08-21 08:46:44 +1200

    Websockets Integration - Fix UT

--- communications/test/dnt/websockets/communications/RequestsTest.java
+++ communications/test/dnt/websockets/communications/RequestsTest.java
@@ -4,22 +4,30 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectReader;
+import com.fasterxml.jackson.databind.jsontype.NamedType;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RequestsTest
 {
-    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper().setSerializationInclusion(JsonInclude.Include.ALWAYS);
-    private static final ObjectReader MESSAGE_READER = OBJECT_MAPPER.readerFor(AbstractRequest.class);
+    private final ObjectMapper mapper;
+    private final ObjectReader messageReader;
+
+    public RequestsTest()
+    {
+        mapper = new ObjectMapper().setSerializationInclusion(JsonInclude.Include.ALWAYS);
+        mapper.registerSubtypes(new NamedType(OptionsRequest.class, OptionsRequest.class.getSimpleName()));
+        messageReader = mapper.readerFor(AbstractRequest.class);
+    }
 
     @Test
     public void testSerde() throws JsonProcessingException
     {
         OptionsRequest original = new OptionsRequest().attachCorrelationId(1);
-        final String json = OBJECT_MAPPER.writeValueAsString(original);
+        final String json = mapper.writeValueAsString(original);
 
-        AbstractRequest decoded = MESSAGE_READER.readValue(json);
+        AbstractRequest decoded = messageReader.readValue(json);
         assertThat(decoded).isInstanceOf(OptionsRequest.class);
     }
 }
